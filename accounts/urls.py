9c2a72c832f55c27c0bf20746fbf5f8d5d465400
Dhruv Joshi <dhruv.joshee@gmail.com>
2017-07-20 19:15:26 +0530

    fixed accounts issues

--- accounts/urls.py
+++ accounts/urls.py
@@ -5,8 +5,10 @@ from . import views
 urlpatterns = [
 	url(r'^user/$', views.UserPage, name='user'),
     url(r'^signup/$', views.SignUp, name='signup'),
-    #{'template_name': } is necessary because by default django searches for templates at register/ , we have to rediirect them to our templates
+    # {'template_name': } is necessary because by default django searches for templates at register/ , we have to rediirect them to our templates
     url(r'^signin/$', views.HomePage,name='signin'),
     url(r'^login/$', auth_views.login,name='login'),
+    # sigin and login urls are different, to sign in the website, go to /signin 
+    # the login url only performs function of logging in using the inbuilt django functions, but it does not have a proper user interface 
     url(r'^logout/$', auth_views.logout,{'next_page': 'Home'},name='logout'),
 ]
